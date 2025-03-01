3d9fbaad81bc11d83f5af49a374d62441a62cd39
PranitRout07 <pranitrout72@gmail.com>
2025-03-02 00:10:02 +0530

    tested email notification

--- main.go
+++ main.go
@@ -3,21 +3,37 @@ package main
 import (
 	"os"
 
+	"github.com/PranitRout07/syncandping/email"
 	"github.com/PranitRout07/syncandping/internal/structure"
-	"github.com/PranitRout07/syncandping/webhook"
+	// "github.com/PranitRout07/syncandping/webhook"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
-	os.Getenv("slack_webhook")
-	w := webhook.NewWebhook("https://hooks.slack.com/services/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")
+	// wh := os.Getenv("slack_webhook")
+	// w := webhook.NewWebhook(wh)
+	// csm := structure.CustomMessage{
+	// 	Message: "Hii",
+	// 	Label: "Normal",
+	// }
+
+	// resp,err := w.SendMessage(csm)
+	// if err!=nil{
+	// 	//
+	// }
+	// _ = resp
+
+	fromEmail := os.Getenv("from_email")
+	toEmail := os.Getenv("to_email")
+	passkey := os.Getenv("passkey")
+
+	e := email.NewEmailNotify(fromEmail,toEmail,passkey)
 	csm := structure.CustomMessage{
-		Message: "Hii",
+		Message: "Testing email notify",
 		Label: "Normal",
 	}
-
-	resp,err := w.SendMessage(csm)
+	resp,err := e.SendMessage(csm)
 	if err!=nil{
 		//
 	}
